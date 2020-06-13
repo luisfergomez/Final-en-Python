3888a7f757d1580d40f637be1b4eccf16a1601ac
luisfergomez <lgomezd4@miumg.edu.gt>
2020-06-13 10:03:10 -0600

    tabla y titulo

--- index.py
+++ index.py
@@ -1,2 +1,18 @@
 from tkinter import ttk
 from tkinter import *
+
+class Operaciones:
+
+    def __init__(self, window):
+        ancho = 700
+        alto = 400
+
+        self.wind = window
+        self.wind.geometry(str(ancho)+'x'+str(alto))
+        self.wind.columnconfigure(0, weight=1)
+        self.wind.title('Exámen Final')
+
+        frame = LabelFrame(self.wind, text = 'Exámen final')
+        frame.grid(row = 0, column = 0, columnspan = 10, pady = 50)
+
+        Label(frame, text = '¡BIENVENIDO!',font=("Arial",33)).grid(row = 0,columnspan= 6)
